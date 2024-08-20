62e78e677fe0ba7f414248afcf9b4e2496716799
BincyAnuz <153153364+BincyAnuz@users.noreply.github.com>
2024-08-20 16:12:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div className='text-3xl font-bold text-green-800 bg-slate-400'>
+    <h1>Hello World</h1>
+    <p>This is the Final project of Namaste React</p>
+    <h3>Let's Build NetflixGPT</h3>
+   </div>
+  )
+}
+
+export default App
