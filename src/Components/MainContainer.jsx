a2382c40c3fb5ae8025d61eca60d04fa58b2ebf3
CKCK666 <vishnuck464@gmail.com>
2024-08-27 18:26:33 +0530

    getMovieVideos function corrected

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -4,24 +4,25 @@ import VideoBackground from "./VideoBackground"
 
 const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies)
-  console.log(movies)
+  
   const mainMovie = movies && movies.length > 0 ? movies[0] : null
-  console.log(mainMovie)
+  
 
   // Check if mainMovie exists before destructuring
-  const {original_title, overview} = mainMovie || {};
-  const movieId = mainMovie ? mainMovie.Id : null;
+  const {original_title, overview,id} = mainMovie || {};
+  const movieId =id
+
 
   // Log original_title and overview to check their values
-  console.log("original_title:", original_title);
-  console.log("overview:", overview);
+  // console.log("original_title:", original_title);
+  // console.log("overview:", overview);
 
 
   return (
     <div>
       {mainMovie ? (
         <>
-          {/* If original_title or overview are objects, convert them to a string */}
+         
           <VideoTitle 
             title={typeof original_title === "object" ? JSON.stringify(original_title) : original_title} 
             overview={typeof overview === "object" ? JSON.stringify(overview) : overview} 
--- src/Components/VideoBackground.jsx
+++ src/Components/VideoBackground.jsx
@@ -4,12 +4,24 @@ import { API_OPTIONS } from "../Utils/constants"
 
 
 
-const VideoBackground = ({movieId}) => {
+const VideoBackground = (movieId) => {
+  let id=movieId.movieId
+  
   const getMovieVideos = async () => {
-    const data = await fetch(('https://api.themoviedb.org/3/movie/533535/videos?language=en-US', API_OPTIONS))
-    const json = await data.json();
-    console.log(json)
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, API_OPTIONS);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json); 
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
   };
+  
   useEffect (() => {
     getMovieVideos()
   }, []);
